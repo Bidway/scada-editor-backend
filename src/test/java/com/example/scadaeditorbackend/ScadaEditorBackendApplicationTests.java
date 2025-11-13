b5d34f85da5ba27883058fd4b1272c35f730103e
David Skvarniuk <davidskvarniuk37@gmail.com>
2025-11-13 17:23:01 +0300

    add

---
+++ src/test/java/com/example/scadaeditorbackend/ScadaEditorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.scadaeditorbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScadaEditorBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
